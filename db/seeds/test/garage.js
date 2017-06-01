b601888f7f3689cedfb40efd2384a02f3af0dff9
Staci McWilliams <stacinicole86@gmail.com>
2017-06-01 10:20:00 -0600

    Add production and test seed files

---
+++ db/seeds/test/garage.js
@@ -0,0 +1,33 @@
+
+exports.seed = function(knex, Promise) {
+  return knex('stuff').del()
+  .then(() => {
+    return Promise.all([
+      knex('stuff').insert({
+        name: 'cat figurines',
+        reason: 'unused',
+        cleanliness: 'rancid'
+      }),
+      knex('stuff').insert({
+        name: 'purses',
+        reason: 'have too many',
+        cleanliness: 'sparkling'
+      }),
+      knex('stuff').insert({
+        name: 'luggage',
+        reason: 'ugly',
+        cleanliness: 'dusty'
+      }),
+      knex('stuff').insert({
+        name: 'books',
+        reason: 'unused',
+        cleanliness: 'dusty'
+      }),
+      knex('stuff').insert({
+        name: 'vhs tapes',
+        reason: 'obsolete',
+        cleanliness: 'dusty'
+      }),
+    ])
+  })
+}
