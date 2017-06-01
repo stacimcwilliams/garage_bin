be83644e8f37cbef137b15ec5044e48a5390ec07
Staci McWilliams <stacinicole86@gmail.com>
2017-06-01 13:23:08 -0600

    Basic enpoint testing complete

--- db/seeds/test/garage.js
+++ db/seeds/test/garage.js
@@ -4,26 +4,31 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return Promise.all([
       knex('stuff').insert({
+        id: 1,
         name: 'cat figurines',
         reason: 'unused',
         cleanliness: 'rancid'
       }),
       knex('stuff').insert({
+        id: 2,
         name: 'purses',
         reason: 'have too many',
         cleanliness: 'sparkling'
       }),
       knex('stuff').insert({
+        id: 3,
         name: 'luggage',
         reason: 'ugly',
         cleanliness: 'dusty'
       }),
       knex('stuff').insert({
+        id: 4,
         name: 'books',
         reason: 'unused',
         cleanliness: 'dusty'
       }),
       knex('stuff').insert({
+        id: 5,
         name: 'vhs tapes',
         reason: 'obsolete',
         cleanliness: 'dusty'
--- test/server-test.js
+++ test/server-test.js
@@ -45,5 +45,69 @@ describe('Server testing', () => {
     })
   })
 
+  describe('GET /api/v1/stuff/:id', () => {
+    it('should retreive one thing by id', (done) => {
+      chai.request(server)
+      .get('/api/v1/stuff/2')
+      .end((error, response) => {
+        response.should.have.status(200)
+        response.should.be.json
+        response.body.should.be.a('array')
+        response.body.should.have.length(1)
+        response.body[0].should.have.property('name')
+        response.body[0].should.have.property('reason')
+        response.body[0].should.have.property('cleanliness')
+        done()
+      })
+    })
+  })
 
+  describe('POST /api/v1/stuff/', () => {
+    it('should create a new item for the garage_bin', (done) => {
+      chai.request(server)
+      .post('/api/v1/stuff')
+      .send({
+        id: '6',
+        name: 'test',
+        reason: 'because',
+        cleanliness: 'rancid'
+      })
+      .end((error, response) => {
+        response.should.have.status(200)
+        response.body.should.be.a('object')
+        response.body.should.have.property('name')
+        response.body.should.have.property('reason')
+        response.body.should.have.property('cleanliness')
+
+        chai.request(server)
+        .get('/api/v1/stuff')
+        .end((err, response) => {
+          response.should.have.status(200)
+          response.body.should.have.length(6)
+          response.should.be.json
+          response.body.should.be.a('array')
+          done()
+        })
+      })
+    })
+  })
+
+  describe('PUT /api/v1/stuff/:id/edit', () => {
+    it('should be able to edit the cleanliness of an item', (done) => {
+      chai.request(server)
+      .get('/api/v1/stuff/2')
+      .end((error, response) => {
+        response.body[0].cleanliness.should.equal('sparkling')
+      })
+      chai.request(server)
+      .put('/api/v1/stuff/2/edit')
+      .send({
+        cleanliness: 'dusty'
+      })
+      .end((error, response) => {
+        response.body[0].cleanliness.should.equal('dusty')
+        done()
+      })
+    })
+  })
 })
