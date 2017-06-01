9119322c41229097a84425a80d9aebd9c15b7fb8
Staci McWilliams <stacinicole86@gmail.com>
2017-06-01 09:53:28 -0600

    Add and configure knex file

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+  test: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL || 'postgres://localhost/garage_bin_test',
+   migrations: {
+     directory: __dirname + '/db/migrations'
+   },
+   seeds: {
+     directory: __dirname + '/db/seeds/test'
+   },
+     useNullAsDefault: true
+ },
+  development: {
+    client: 'pg',
+    connection: 'postgress://localhost/garage_bin',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+      useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+    directory: './db/seeds/production'
+  },
+    useNullAsDefault: true
+  }
+};
--- server.js
+++ server.js
@@ -1,2 +1,31 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+
+const app = express();
+
+const enviroment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[enviroment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'GarageBin';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('lib'));
+
+app.get('/', (request, response ) => {
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
+});
+
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+  });
+}
+
+module.exports = app;
